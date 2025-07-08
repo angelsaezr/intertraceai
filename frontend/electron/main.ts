1d7d62fae684f46c032b009b02949a5887475eb5
Angel <angelsaezrs@gmail.com>
2025-07-08 21:18:25 +0200

    Update icon file extension from .svg to .png in BrowserWindow

--- frontend/electron/main.ts
+++ frontend/electron/main.ts
@@ -30,7 +30,7 @@ let win: BrowserWindow | null;
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "network.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "network.png"),
     width: 800,
     height: 600,
     minWidth: 800,
